c76f5a86217c02db8236c7113da25a1c01bb0918
imhossainahmed <hossain.contact.main@gmail.com>
2026-03-25 20:32:45 +0600

    Updated The All Page Link

--- src/components/TrendingApp.jsx
+++ src/components/TrendingApp.jsx
@@ -10,7 +10,7 @@ const TrendingApp = () => {
     <div className="w-11/12 mx-auto space-y-12">
       <div className="text-center space-y-4">
         <h2 className="text-5xl font-black text-[#1E293B]">Trending Apps</h2>
-        <p className="text-lg text-base-content/60">Explore All Trending Apps on the Market developed by us</p>
+        <p className="text-lg text-base-content/60">Explore All Trending Apps on the Market Place developed by us</p>
       </div>
       
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
